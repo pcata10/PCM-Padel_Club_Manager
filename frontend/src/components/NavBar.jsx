de22ce687e4a8004909b05d10a4a81adc996b727
Paolo Cataleta <paolocataleta@icloud.com>
2026-03-11 16:04:50 +0100

    ctlatianov8

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -120,7 +120,7 @@ export default function NavBar() {
             {role === "admin" && (
               <Link
                 to="/report"
-                className="px-6 py-2 bg-indigo-400 text-white rounded-xl font-semibold shadow-md hover:bg-indigo-600 hover:-translate-y-0.5 transition-all"
+                className="flex items-center gap-3 px-4 py-3 bg-indigo-400 text-white rounded-xl font-semibold shadow-md hover:bg-indigo-600  transition-all"
               >
                 Report
               </Link>
